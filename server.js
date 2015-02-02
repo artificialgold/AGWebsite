47f71640461c80886a464d648cdd7a60318f2bb6
Andrew Metcalfe <andrew.metcalfe@artificialgold.com>
2015-02-02 22:37:20 +0000

    upgraded to Hapi

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+// var http = require('http')
+// var port = process.env.PORT || 1337;
+// http.createServer(function(req, res) {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('Hello World\n');
+// }).listen(port);
+
+
+var Hapi = require('hapi'); 
+
+// Create a new server
+var server = new Hapi.Server();
+
+// Setup the server with a host and port
+server.connection({port: 1337});
+
+server.route({ method: 'GET', path: '/', handler: function(request, reply) { reply('Hello world, Hapi'); } });
+
+server.start(function() { console.log('Hapi is listening to http://localhost:1337'); });
+
+
