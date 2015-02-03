ba4d0192420b72e2081cd1b8df1454a92381165e
Andrew <andrew.metcalfe@artificialgold.com>
2015-02-03 12:09:37 +0000

    got my website working on /test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 //   res.end('Hello World\n');
 // }).listen(port);
 
-
+var Path = require('path');
 var Hapi = require('hapi'); 
 
 // Create a new server
@@ -17,7 +17,43 @@ var port = process.env.PORT || 1337;
 // Setup the server with a host and port
 server.connection({port: port});
 
-server.route({ method: 'GET', path: '/', handler: function(request, reply) { reply('Hello world, Hapi'); } });
+server.views({
+    engines: {
+        html: require('handlebars')
+    },
+    relativeTo: __dirname,
+    path: "./views"
+});
+
+server.route({ method: 'GET', path: '/', 
+	handler: function(request, reply) 
+	{ reply('Hello world, Hapi'); } });
+
+server.route({ method: 'GET', path: '/test', 
+	handler: function(request, reply)
+	{ reply.view("test"); } });
+
+server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+        directory: {
+            path: 'content',
+            listing: true
+        }
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/content/{param*}',
+    handler: {
+        directory: {
+            path: 'content',
+            listing: true
+        }
+    }
+});
 
 server.start(function() { console.log('Hapi is listening to http://localhost:1337'); });
 
